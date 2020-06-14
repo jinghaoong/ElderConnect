0179c2be914413a0fe00052232869fa39d5da062
Jing Hao Ong <jinghaoong@gmail.com>
2020-06-15 02:01:19 +0800

    Fixed reminder times to only appear if exist

--- elderconnect/elderconnect/settings.py
+++ elderconnect/elderconnect/settings.py
@@ -111,11 +111,11 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Singapore'
 
-TIME_INPUT_FORMATS = ('%H:%M %p',)
+TIME_INPUT_FORMATS = ('%H:%M',)
 
 USE_I18N = True
 
-USE_L10N = False
+USE_L10N = True
 
 USE_TZ = True
 
