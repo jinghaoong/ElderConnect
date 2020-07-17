d659ee8c12f5516100be874cccbdbb32bc053de9
Jing Hao Ong <jinghaoong@gmail.com>
2020-07-17 18:46:52 +0800

    Added basic tts functionality to active reminders

--- mobile/lib/pages/reminders.dart
+++ mobile/lib/pages/reminders.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 import 'package:mobile/main.dart';
 import 'package:mobile/pages/reminder_detail.dart';
@@ -17,75 +20,125 @@ class RemindersPage extends StatefulWidget {
   _RemindersPageState createState() => _RemindersPageState();
 }
 
-class Reminder {
-  final int pk;
-  final String title;
-  final String description;
-  final String currentDate;
-  final String endDate;
-  final String time1;
-  final String time2;
-  final String time3;
-  final String time4;
-  final String imgUrl;
-
-  Reminder({
-    this.pk, this.title, this.description, this.currentDate, this.endDate,
-    this.time1, this.time2, this.time3, this.time4, this.imgUrl
-  });
-
-  factory Reminder.fromJson(Map<String, dynamic> json) {
-    return Reminder(
-      pk: json['id'],
-      title: json['title'],
-      description: json['description'],
-      currentDate: json['date_current'],
-      endDate: json['date_end'],
-      time1: json['time_1'],
-      time2: json['time_2'],
-      time3: json['time_3'],
-      time4: json['time_4'],
-      imgUrl: json['medicine_image'],
-    );
-  }
-}
-
-Future<List<Reminder>> fetchReminders() async {
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  String token = sharedPreferences.get("token");
-
-  var jsonData;
-  var url = 'http://$host:8000/api/api_reminders/';
-  var response = await http.get(
-      url,
-      headers: {"Authorization": "Token " + token,}
-  );
-
-  if (response.statusCode == 200) {
-    return jsonData = (jsonDecode(response.body) as List)
-        .map((data) => Reminder.fromJson(data))
-        .toList();
-  }
-  else {
-    throw Exception("Unable to retrieve Reminders");
-  }
-}
-
-String timeString(String t1, String t2, String t3, String t4) {
-  String times = t1;
-  if (t2 != null) times += '  |  ' + t2;
-  if (t3 != null) times += '\n' + t3;
-  if (t4 != null) times += '  |  ' + t4;
-
-  return times;
-}
+enum TtsState { playing, stopped, paused, continued }
 
 class _RemindersPageState extends State<RemindersPage> {
+
+  FlutterTts flutterTts;
+  String language = 'en-US';
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.2;
+
+  String _voiceText;
+
+  TtsState ttsState = TtsState.stopped;
 
   ScrollController controller;
   bool showFab = true;
   Future<List<Reminder>> _remindersFuture;
   bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _remindersFuture = fetchReminders();
+    controller = ScrollController();
+    controller.addListener(() {
+      setState(() {
+        showFab = controller.position.userScrollDirection == ScrollDirection.forward;
+      });
+    });
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    if(!kIsWeb) {
+      if (Platform.isAndroid) {
+        _getEngines();
+      }
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    if (kIsWeb || Platform.isIOS) {
+      flutterTts.setPauseHandler(() {
+        setState(() {
+          print("Paused");
+          ttsState = TtsState.paused;
+        });
+      });
+
+      flutterTts.setContinueHandler(() {
+        setState(() {
+          print("Continued");
+          ttsState = TtsState.continued;
+        });
+      });
+    }
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getEngines() async {
+    var engines = await flutterTts.getEngines;
+    if (engines != null) {
+      for (dynamic engine in engines) {
+        print(engine);
+      }
+    }
+  }
+
+  Future _speak() async {
+    if (_voiceText != null) {
+      if (_voiceText.isNotEmpty) {
+        var result = await flutterTts.speak(_voiceText);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
 
   _deleteReminder(int id) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
@@ -110,25 +163,10 @@ class _RemindersPageState extends State<RemindersPage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    _remindersFuture = fetchReminders();
-    controller = ScrollController();
-    controller.addListener(() {
-      setState(() {
-        showFab = controller.position.userScrollDirection == ScrollDirection.forward;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-            'Reminders',
-            style: TextStyle(fontSize: 25.0)
-        ),
+        title: Text('Reminders', style: TextStyle(fontSize: 25.0)),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
@@ -150,9 +188,19 @@ class _RemindersPageState extends State<RemindersPage> {
             future: _remindersFuture,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
+                int numItems = snapshot.data.length;
+                if (numItems == 0) {
+                  return Center(
+                    child: Text(
+                        'No active reminders!',
+                        style: TextStyle(fontSize: 30.0),
+                        textAlign: TextAlign.center,
+                    ),
+                  );
+                }
                 return ListView.builder(
                     controller: controller,
-                    itemCount: snapshot.data.length,
+                    itemCount: numItems,
                     itemBuilder: (context, index) {
                       Reminder reminder = snapshot.data[index];
                       String times = timeString(reminder.time1,
@@ -160,6 +208,15 @@ class _RemindersPageState extends State<RemindersPage> {
                           reminder.time3,
                           reminder.time4);
                       return ListTile(
+                        leading: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _voiceText = reminder.title + '|' + times;
+                            });
+                            _speak();
+                          },
+                          icon: Icon(Icons.hearing),
+                        ),
                         onTap: () {
                           Navigator.push(
                               context,
@@ -175,7 +232,7 @@ class _RemindersPageState extends State<RemindersPage> {
                           child: Text(
                               reminder.title,
                               style: TextStyle(
-                                  fontSize: 20.0
+                                  fontSize: 18.0
                               )
                           ),
                         ),
@@ -251,5 +308,68 @@ class _RemindersPageState extends State<RemindersPage> {
       ),
     );
   }
+}
+
+class Reminder {
+  final int pk;
+  final String title;
+  final String description;
+  final String currentDate;
+  final String endDate;
+  final String time1;
+  final String time2;
+  final String time3;
+  final String time4;
+  final String imgUrl;
+
+  Reminder({
+    this.pk, this.title, this.description, this.currentDate, this.endDate,
+    this.time1, this.time2, this.time3, this.time4, this.imgUrl
+  });
+
+  factory Reminder.fromJson(Map<String, dynamic> json) {
+    return Reminder(
+      pk: json['id'],
+      title: json['title'],
+      description: json['description'],
+      currentDate: json['date_current'],
+      endDate: json['date_end'],
+      time1: json['time_1'],
+      time2: json['time_2'],
+      time3: json['time_3'],
+      time4: json['time_4'],
+      imgUrl: json['medicine_image'],
+    );
+  }
+}
+
+Future<List<Reminder>> fetchReminders() async {
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  String token = sharedPreferences.get("token");
+
+  var jsonData;
+  var url = 'http://$host:8000/api/api_reminders/';
+  var response = await http.get(
+      url,
+      headers: {"Authorization": "Token " + token,}
+  );
+
+  if (response.statusCode == 200) {
+    return jsonData = (jsonDecode(response.body) as List)
+        .map((data) => Reminder.fromJson(data))
+        .toList();
+  }
+  else {
+    throw Exception("Unable to retrieve Reminders");
+  }
+}
+
+String timeString(String t1, String t2, String t3, String t4) {
+  String times = t1;
+  if (t2 != null) times += '  |  ' + t2;
+  if (t3 != null) times += '\n' + t3;
+  if (t4 != null) times += '  |  ' + t4;
+
+  return times;
 }
 
